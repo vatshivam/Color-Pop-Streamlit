2b51eac057a745a322c40b176b163a39ffd5057f
Shivam Vats <shivam88y@yahoo.com>
2023-11-28 13:38:21 -0700

    Added project pages

---
+++ pages/4_Results.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.set_page_config(page_title="Results", page_icon=":bar_chart:")
+
+# st.markdown(
+#         """
+#         <style>
+#             [data-testid="stSidebarNav"]::before {
+#                 content: "Color Pop";
+#                 margin-left: 20px;
+#                 margin-top: 20px;
+#                 font-size: 30px;
+#                 position: relative;
+#                 top: 50px;
+#                 bottom: 50px;
+#             }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
+
+with st.sidebar:
+        st.image('./assets/icon.jpg')
+        st.title("Color Pop")
+        st.subheader("Human segmentation application making color-pop effect easy to use!")
+
+st.markdown("# Results :bar_chart:")
+
+st.write(
+    """This demo illustrates a combination of plotting and animation with
+Streamlit. We're generating a bunch of random numbers in a loop for around
+5 seconds. Enjoy!"""
+)
